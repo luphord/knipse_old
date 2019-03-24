995b9664594ec9c60e32c622a5f96b776cb06e1e
luphord <luphord@protonmail.com>
2019-03-24 13:04:50 +0100

    type annotation for ImageDescriptor

--- knipse/descriptor.py
+++ knipse/descriptor.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
+from datetime import datetime
 
 
 class ImageDescriptor:
     '''Container for image metadata'''
 
-    def __init__(self, path, creation_date, dhash):
+    def __init__(self,
+                 path: Path,
+                 creation_date: datetime,
+                 dhash: bytes) -> None:
         self.path = Path(path)
         self.creation_date = creation_date
         self.dhash = dhash
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '''ImageDescriptor('{}', {!r}, {})'''.format(
             self.path, self.creation_date, self.dhash)
