c8043e1c5a2ab653b08631fbf157689abea9a999
luphord <luphord@protonmail.com>
2019-03-25 06:43:48 +0100

    second regression test

--- tests/test_descriptor.py
+++ tests/test_descriptor.py
@@ -15,6 +15,7 @@ class TestImageDescriptor(unittest.TestCase):
     def setUp(self):
         self.src = Path(__file__).resolve().parent / 'images'
         self.path = self.src / 'forest' / 'forest_snow.jpg'
+        self.path2 = self.src / 'photo01.jpg'
 
     def test_descriptor_regression(self):
         '''Regression test for ImageDescriptor of a particular image'''
@@ -29,4 +30,16 @@ class TestImageDescriptor(unittest.TestCase):
         self.assertEqual(expected.created_at, actual.created_at)
         # do not compare modification times of images in version control
         # as these depend on the time of checkout
+        self.assertEqual(expected.dhash, actual.dhash)
+        # test a second image (this time without exif data)
+        expected = ImageDescriptor(
+                    Path('photo01.jpg'),
+                    None,
+                    datetime(2019, 3, 22, 12, 33, 6, 589350),
+                    b'\xf1\xf8\xf8\xf1\xfc\xfc\xf4\xf5\x08\xf1\xec' +
+                    b'\x00\x19\xff\xfe\xfc')
+        actual = descriptor_from_image(self.src, self.path2,
+                                       Image.open(self.path2))
+        self.assertEqual(expected.path, actual.path)
+        self.assertEqual(expected.created_at, actual.created_at)
         self.assertEqual(expected.dhash, actual.dhash)
