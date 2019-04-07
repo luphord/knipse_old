01db97e091e9551e34888e22dcd67e41a0e6bc89
luphord <luphord@protonmail.com>
2019-04-07 10:37:24 +0200

    extract to class

--- knipse/gui.py
+++ knipse/gui.py
@@ -9,6 +9,21 @@ from PIL import ImageTk, Image
 import click
 
 
+class ImageDisplay(ttk.Frame):
+
+    def __init__(self, parent, path):
+        super().__init__(master=parent, padding='3 3 12 12')
+        self.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+        img = Image.open(path)
+        img = img.resize((800, 600))
+
+        self.tkimg = ImageTk.PhotoImage(img)
+
+        canvas = tk.Canvas(self)
+        canvas.pack()
+        canvas.create_image(100, 100, image=self.tkimg)
+
+
 @click.command(name='display')
 @click.argument('path',
                 type=click.Path(exists=True, file_okay=True, dir_okay=False,
@@ -18,16 +33,6 @@ def cli_display(path):
     root = tk.Tk()
     root.title('knipse - display {}'.format(path))
 
-    mainframe = ttk.Frame(root, padding="3 3 12 12")
-    mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-
-    img = Image.open(path)
-    img = img.resize((800, 600))
-
-    tkimg = ImageTk.PhotoImage(img)
-
-    canvas = tk.Canvas(mainframe)
-    canvas.pack()
-    canvas.create_image(100, 100, image=tkimg)
+    ImageDisplay(root, path)
 
     root.mainloop()
