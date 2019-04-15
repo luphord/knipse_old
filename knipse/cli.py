4fe41330c67059906723d5b66bb3c27d1401b18c
luphord <luphord@protonmail.com>
2019-04-15 17:24:10 +0200

    first version of scan command

--- knipse/cli.py
+++ knipse/cli.py
@@ -8,6 +8,7 @@ import click
 from .db import KnipseDB
 from .dhash import cli_dhash
 from .walk import cli_walk
+from .scan import cli_scan
 from .symlink import cli_symlink
 from .gui import cli_display
 
@@ -38,6 +39,7 @@ def cli_list(ctx):
 
 cli_knipse.add_command(cli_dhash)
 cli_knipse.add_command(cli_walk)
+cli_knipse.add_command(cli_scan)
 cli_knipse.add_command(cli_symlink)
 cli_knipse.add_command(cli_display)
 cli_knipse.add_command(cli_list)
---
+++ knipse/scan.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from typing import List, Callable, Optional, Iterable, Tuple  # noqa: 401
+
+import click
+
+from .db import KnipseDB
+from .walk import walk_images
+from .image import descriptor_from_image
+
+
+def scan_images(db: KnipseDB, base_folder: Path) \
+        -> Iterable[Tuple[Path, float]]:
+    '''Walk all folders below `base_folder`
+       and store contained images in database
+    '''
+    filter = db.get_known_images_filter()
+    for file_path, img, progress in walk_images(base_folder, filter):
+        descr = descriptor_from_image(base_folder, file_path, img)
+        db.store(descr)
+        yield file_path, progress
+
+
+@click.command(name='scan')
+@click.argument('base_folder',
+                type=click.Path(exists=True, file_okay=False, dir_okay=True,
+                                resolve_path=True), default='.')
+@click.pass_context
+def cli_scan(ctx, base_folder):
+    '''Walk all folders below `base_folder`
+       and store contained images in database
+    '''
+    db = ctx.obj['database']
+    for file_path, progress in scan_images(db, base_folder):
+        click.echo('{:.1f}%\t{}'.format(progress * 100, file_path))
