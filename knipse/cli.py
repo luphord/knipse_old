ba49396ee1df723f9d8cb1a590d14e1c51d6af5b
luphord <luphord@protonmail.com>
2019-05-06 17:14:05 +0200

    database and source can be read from environment variables

--- knipse/cli.py
+++ knipse/cli.py
@@ -18,17 +18,21 @@ from .gui import cli_display
               type=click.Path(file_okay=True, dir_okay=False,
                               resolve_path=True),
               default=os.path.join(os.path.expanduser('~'), '.knipse.sqlite'),
+              envvar='KNIPSE_DATABASE',
               show_default=True,
               help='Knipse database to use')
 @click.option('-s', '--source',
               type=click.Path(exists=True, file_okay=False, dir_okay=True,
                               resolve_path=True),
               default=os.path.expanduser('~'),
+              envvar='KNIPSE_SOURCE',
               show_default=True,
               help='base folder of image source')
 @click.pass_context
 def cli_knipse(ctx, database, source):
     '''Manage your photo collections and lists.'''
+    click.echo('Starting knipse with database {} and image source {}'
+               .format(database, source))
     ctx.ensure_object(dict)
     ctx.obj['database'] = KnipseDB(database)
     ctx.obj['source'] = source
