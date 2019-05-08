a8cf4d9cfb4263e019f101bdf22b11fb7639f0cd
luphord <luphord@protonmail.com>
2019-05-08 17:16:35 +0200

    logging can be toggled by cli switch -v

--- knipse/cli.py
+++ knipse/cli.py
@@ -14,7 +14,7 @@ from .symlink import cli_symlink
 from .gui import cli_display
 
 
-logging.config.dictConfig({
+_DEFAULT_LOGGING_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
@@ -35,7 +35,7 @@ logging.config.dictConfig({
             'level': 'DEBUG'
         }
     }
-})
+}
 
 
 @click.group(name='knipse')
@@ -45,22 +45,26 @@ logging.config.dictConfig({
               default=os.path.join(os.path.expanduser('~'), '.knipse.sqlite'),
               envvar='KNIPSE_DATABASE',
               show_default=True,
-              help='Knipse database to use')
+              help='Knipse database to use.')
 @click.option('-s', '--source',
               type=click.Path(exists=True, file_okay=False, dir_okay=True,
                               resolve_path=True),
               default=os.path.expanduser('~'),
               envvar='KNIPSE_SOURCE',
               show_default=True,
-              help='base folder of image source')
+              help='Base folder of image source.')
+@click.option('-v', '--verbose/--no-verbose', default=False,
+              help='Show detailed log messages.')
 @click.pass_context
-def cli_knipse(ctx, database, source):
+def cli_knipse(ctx, database, source, verbose):
     '''Manage your photo collections and lists.'''
     click.echo('Starting knipse with database {} and image source {}'
                .format(database, source))
     ctx.ensure_object(dict)
     ctx.obj['database'] = KnipseDB(database)
     ctx.obj['source'] = source
+    if verbose:
+        logging.config.dictConfig(_DEFAULT_LOGGING_CONFIG)
     return 0
 
 
