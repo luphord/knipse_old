5b61d2bc95c51b4100018a8ecbca233cada164df
luphord <luphord@protonmail.com>
2019-05-14 17:34:02 +0200

    show all images if not list is specified

--- knipse/show.py
+++ knipse/show.py
@@ -4,6 +4,7 @@ import click
 
 from .descriptor import ListDescriptor
 
+
 @click.command(name='show')
 @click.argument('knipse-object', type=click.STRING, nargs=-1)
 @click.pass_context
@@ -13,4 +14,7 @@ def cli_show(ctx, knipse_object):
     lists = [obj[1:] for obj in knipse_object if obj.upper().startswith('L')]
     for list_id in lists:
         for img in db.list_images(ListDescriptor(list_id, None, '')):
+            click.echo(img)
+    if not knipse_object:
+        for img in db.list_images():
             click.echo(img)
