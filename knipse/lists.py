555942e3c1cd37d961bd19344ce5362c30fb6114
luphord <luphord@protonmail.com>
2019-07-17 17:30:38 +0200

    move show-image subcommand to list subcommand

--- knipse/lists.py
+++ knipse/lists.py
@@ -6,6 +6,7 @@ import click
 
 from .descriptor import ListDescriptor, ListEntryDescriptor
 from .db import ImageRecognizer
+from .util import FIELDS
 
 
 def image_id_from_string(image_str: str,
@@ -68,5 +69,31 @@ def cli_append_to_list(ctx, images, list_id):
         db.store_list_entry(entry_descr)
 
 
+@click.command(name='show')
+@click.option('-f', '--fields', type=FIELDS,
+              default='list_id;list_entry_id;image_id;position;path',
+              show_default=True,
+              help='fields of knipse object to output')
+@click.option('-h', '--header/--no-header', default=False,
+              show_default=True,
+              help='print column headers')
+@click.argument('list-id', type=click.STRING, nargs=-1)
+@click.pass_context
+def cli_show_list(ctx, fields, header, list_id):
+    '''Show lists corresponding to `list_id`(s)'''
+    db = ctx.obj['database']
+    list_ids = [int(obj[1:] if obj.upper().startswith('L') else int(obj))
+                for obj in list_id]
+    lists = [list(db.load_list_entries(ListDescriptor(list_id, None, '')))
+             for list_id in list_ids]
+    first_img = lists[0][0] if lists and lists[0] else []
+    if header:
+        click.echo(fields.headers_tab(*first_img))
+    for lst in lists:
+        for list_entry, img in lst:
+            click.echo(fields.tab(list_entry, img))
+
+
 cli_list.add_command(cli_create_list)
 cli_list.add_command(cli_append_to_list)
+cli_list.add_command(cli_show_list)
--- knipse/show.py
+++ knipse/show.py
@@ -2,33 +2,7 @@
 
 import click
 
-from .descriptor import ListDescriptor
 from .util import FIELDS
-
-
-@click.command(name='show-list')
-@click.option('-f', '--fields', type=FIELDS,
-              default='list_id;list_entry_id;image_id;position;path',
-              show_default=True,
-              help='fields of knipse object to output')
-@click.option('-h', '--header/--no-header', default=False,
-              show_default=True,
-              help='print column headers')
-@click.argument('list-id', type=click.STRING, nargs=-1)
-@click.pass_context
-def cli_show_list(ctx, fields, header, list_id):
-    '''Show lists corresponding to `list_id`(s)'''
-    db = ctx.obj['database']
-    list_ids = [int(obj[1:] if obj.upper().startswith('L') else int(obj))
-                for obj in list_id]
-    lists = [list(db.load_list_entries(ListDescriptor(list_id, None, '')))
-             for list_id in list_ids]
-    first_img = lists[0][0] if lists and lists[0] else []
-    if header:
-        click.echo(fields.headers_tab(*first_img))
-    for lst in lists:
-        for list_entry, img in lst:
-            click.echo(fields.tab(list_entry, img))
 
 
 @click.command(name='show-image')
