a2471906ab5675a28aa1dcc87ab80cd3f3f6fe4d
luphord <luphord@protonmail.com>
2019-07-22 06:59:42 +0200

    add a basic treeview

--- knipse/gui/window.py
+++ knipse/gui/window.py
@@ -8,6 +8,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.treeview import TreeView, TreeViewLabel
 
 from ..db import KnipseDB
 
@@ -23,6 +24,10 @@ class KnipseApp(App):
 
     def build(self):
         layout = GridLayout(cols=1, padding=10)
+        tree = TreeView(root_options=dict(text='Collection'))
+        tree.add_node(TreeViewLabel(text='My first item'))
+        tree.add_node(TreeViewLabel(text='My second item'))
+        layout.add_widget(tree)
         for img in islice(self.db.load_all_images(), 10):
             text = '{} {}'.format(img.image_id, img.path)
             layout.add_widget(Label(text=text))
