2a5344889aa70787014d5b9cf3ba86f306d77aa8
luphord <luphord@protonmail.com>
2019-07-23 17:40:45 +0200

    move build logic to kv file

--- knipse/gui/window.py
+++ knipse/gui/window.py
@@ -8,10 +8,9 @@ from pathlib import Path
 
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.treeview import TreeView, TreeViewLabel
+from kivy.properties import ObjectProperty, StringProperty
 
 from ..db import KnipseDB
 from ..image import ImageDescriptor
@@ -21,13 +20,14 @@ kivy.require('1.11.0')
 
 
 class FolderTreeWidget(FloatLayout):
+    root_label = StringProperty('')
+    db = ObjectProperty(None)
 
-    def __init__(self,
-                 root_label: str,
-                 db: KnipseDB,
-                 *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._populate(root_label, db.load_all_images())
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def on_db(self, *args):
+        self._populate(self.root_label, self.db.load_all_images())
 
     def _populate(self,
                   root_label: str,
@@ -49,14 +49,3 @@ class KnipseApp(App):
     def __init__(self, db: KnipseDB, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.db = db
-
-    def build(self):
-        master_layout = BoxLayout(orientation='horizontal', padding=10)
-        tree = FolderTreeWidget('My Collection', self.db, size_hint=(0.3, 1))
-        master_layout.add_widget(tree)
-        images_layout = BoxLayout(orientation='vertical', size_hint=(0.7, 1))
-        master_layout.add_widget(images_layout)
-        for img in islice(self.db.load_all_images(), 5):
-            text = '{} {}'.format(img.image_id, img.path)
-            images_layout.add_widget(Label(text=text))
-        return master_layout
