d7000885e528f78ab961e9bdfa5f862a4007b00f
luphord <luphord@protonmail.com>
2019-07-23 17:44:05 +0200

    include subpackages

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     include_package_data=True,
     keywords='knipse',
     name='knipse',
-    packages=find_packages(include=['knipse']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
